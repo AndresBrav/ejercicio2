dd1e43f81dbdc2410c189cf92b6167cf72bb03d4
EdsonCespedesD <145913142+EdsonCespedesD@users.noreply.github.com>
2023-09-28 23:52:19 -0400

    division

--- aritmetica.py
+++ aritmetica.py
@@ -9,3 +9,14 @@ resultado = a + b
 
 # Imprimir el resultado
 print(resultado)
+
+dividendo = float(input("Ingrese el dividendo: "))
+divisor = float(input("Ingrese el divisor: "))
+
+if divisor == 0:
+    print("¡Error! No puedes dividir por cero.")
+else:
+    resultado = dividendo / divisor
+    print(f"El resultado de dividir {dividendo} entre {divisor} es: {resultado}")
+
+#Edson Gabriel Cespedes Delgadillo
